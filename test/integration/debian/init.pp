1ba246118347cb07aebe8f0d6a7a8fb3fc1f869b
Antoine MILLET <amillet@vente-privee.com>
2017-11-22 14:47:44 +0100

    Fix mirror

--- test/integration/debian/init.pp
+++ test/integration/debian/init.pp
@@ -5,10 +5,21 @@ exec { 'apt-get update':
   path    => [ '/usr/bin', '/usr/sbin', '/bin', '/sbin' ],
 }
 
-class { 'influxdb':
-  apt_location => 'http://mirror.vpgrp.io/debian-influxdb',
-  package      => true,
-  service      => true,
-  require      => Exec['apt-get update'],
+if ($::lsbdistid == 'Debian') {
+  class { 'influxdb':
+    apt_location => 'http://mirror.vpgrp.io/debian-influxdb',
+    package      => true,
+    service      => true,
+    require      => Exec['apt-get update'],
+  }
+}
+
+if ($::lsbdistid == 'Ubuntu') {
+  class { 'influxdb':
+    apt_location => 'http://mirror.vpgrp.io/ubuntu-influxdb',
+    package      => true,
+    service      => true,
+    require      => Exec['apt-get update'],
+  }
 }
 # EOF
